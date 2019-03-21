12462dcbee63d0b01ad949975f7d2dcd1f96e856
Raphael Nunes <raphael@v2v.net>
2019-03-21 18:37:58 -0300

    - Fixed google-analytics inicialization

--- addon/metrics-adapters/google-analytics.js
+++ addon/metrics-adapters/google-analytics.js
@@ -8,8 +8,6 @@ import canUseDOM from '../utils/can-use-dom';
 import { compact } from '../utils/object-transforms';
 import BaseAdapter from './base';
 
-let isStarted = false;
-
 export default BaseAdapter.extend({
   toStringExtension() {
     return 'GoogleAnalytics';
@@ -27,15 +25,13 @@ export default BaseAdapter.extend({
     const hasOptions = isPresent(Object.keys(config));
 
     if (canUseDOM) {
-      if (!isStarted) {
+      if ($('script[src*="google-analytics"]').length === 0) {
         /* jshint ignore:start */
         (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
           (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
           m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
         })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
         /* jshint ignore:end */
-
-        isStarted = true;
       }
 
       let params = ['create', id, 'auto'];
